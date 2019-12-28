25f1eedad2a977d9258646d0a9b852493a921e78
Ivan Tokar <i.j.tokar@gmail.com>
2019-12-28 21:09:58 +0200

    Environment (configs, router, tests, logs, Make)

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,56 @@
+package apiserver
+
+import (
+    "github.com/gorilla/mux"
+    "github.com/sirupsen/logrus"
+    "io"
+    "net/http"
+)
+
+type APIServer struct {
+    config *Config
+    logger *logrus.Logger
+    router *mux.Router
+}
+
+func New(config *Config) *APIServer {
+    return &APIServer {
+        config: config,
+        logger: logrus.New(),
+        router: mux.NewRouter(),
+    }
+}
+
+func (s *APIServer) Start() error {
+    if err := s.configureLogger(); err != nil {
+        return err
+    }
+
+    s.configureRouter()
+
+    s.logger.Info( "starting API server")
+
+    return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configureLogger() error {
+    level, err := logrus.ParseLevel(s.config.LogLevel)
+
+    if err != nil {
+        return err
+    }
+
+    s.logger.SetLevel(level)
+
+    return nil
+}
+
+func (s *APIServer) configureRouter() {
+    s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+    return func(writer http.ResponseWriter, request *http.Request) {
+        io.WriteString(writer, "Hello")
+    }
+}
